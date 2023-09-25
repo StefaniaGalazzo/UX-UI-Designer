054b6e49e4d8e096da20a6ce586d149f95985ec1
Stefania <galazzostefania@gmail.com>
2023-09-25 10:09:29 +0200

    test fix btn

--- app/_components/UI/MainComp/MainComp.jsx
+++ app/_components/UI/MainComp/MainComp.jsx
@@ -12,7 +12,7 @@ export default function MainComp() {
       <div className={styles.main} id="top">
         {/* <Circle /> */}
         <div className={styles.texts}>
-          {/* <h1 className={styles.description}>
+          <h1 className={styles.description}>
             Hi! I&apos;m a<span className={styles.enphasis}> UI Designer </span>
             with strong interest in
             <span className={styles.enphasis}> Accessibility</span>
@@ -21,9 +21,8 @@ export default function MainComp() {
             I always care about people. Like UI designer i cant image to create
             a design that dont fit with the accessibility web standars.
             Fortunally, I&apos;ve a big passion for frontend developing
-          </h3> */}
+          </h3>
           <div className={styles.cta}>
-            <ToTopBtn href={"/#projects"} toPosition />
             <ButtonsShadow txt={"Projects"} href={"/#projects"} />
             <ButtonsShadow txt={"Skills"} href={"/skills"} blank />
             <ButtonsShadow
--- app/_components/atoms/ToTopBtn/ToTopBtn.jsx
+++ app/_components/atoms/ToTopBtn/ToTopBtn.jsx
@@ -4,7 +4,7 @@ import styles from "./ToTopBtn.module.scss";
 export default function ToTopBtn({ href, toTOP, toPosition }) {
   return (
     <a className={toTOP ? styles.toTOPbtn : styles.toPosition} href={href}>
-      <BiArrowFromBottom />
+      {toTOP && <BiArrowFromBottom />}
     </a>
   );
 }
